abc836c6d93869d65b4ba6fa223ef6ef469c3268
MomchilAngelov0040 <MomchilAngelov0040@gmail.com>
2020-11-14 00:18:23 +0200

    Show categories with dummy data

--- Xamply/Web/Xamply.Web/ClientApp/src/App.js
+++ Xamply/Web/Xamply.Web/ClientApp/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout'
-import { Dashboard } from './components/Dashboard'
+import Dashboard from './components/Dashboard'
 
 export default class App extends React.Component {
-  static displayName = App.name;
-
   render() {
     return (
       <Layout>
--- Xamply/Web/Xamply.Web/ClientApp/src/index.js
+++ Xamply/Web/Xamply.Web/ClientApp/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { rootReducer } from './store/roodReducer'
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter basename={baseUrl}>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   rootElement
 );
